9cabdf1d643c50ecb6860e8f61d4bf8d28969afe
m.jawad_VICTUS <m.jawad@smartsoft-me.com>
2023-07-03 16:47:21 +0300

    Added movie class, fixed some nullability checks with addition of arrow functions

---
+++ Movies/Movie.cs
@@ -0,0 +1,24 @@
+﻿namespace Movies
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Genre { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public Movie(int id, string name, string genre, int year)
+        {
+            Name = name;
+            Genre = genre;
+            Year = year;
+            Id = id;
+        }
+
+        public void Update(string name, string genre, int year)
+        {
+            Name = name;
+            Genre = genre;
+            Year = year;
+        }
+    }
+}
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -1,145 +1,124 @@
-﻿namespace Movies
+﻿
+using Movies;
+
+int choice;
+List<Movie> movies = new();
+
+
+void PrintOptions()
 {
-    internal class Movie
+    Console.WriteLine("Please Choose the action you want to perform !");
+    Console.WriteLine("1: Create item");
+    Console.WriteLine("2: Update item");
+    Console.WriteLine("3: Delete item");
+    Console.WriteLine("4: Display item");
+    Console.WriteLine("5: Exit \n");
+}
+
+do
+{
+    PrintOptions();
+    choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
     {
-
-        public string genre { get; set; }
-
-        public string name { get; set; }
-
-        public int year { get; set; }
-
-        public Movie(string name, string genre, int year)
-        {
-            this.name = name;
-            this.genre = genre;
-            this.year = year;
-
-
-        }
-
-
-
-
-        public static void Main(string[] args)
-        {
-            int choice;
-            List<Movie> list = new List<Movie>();
-
-
-
-
-            void printOptions()
+        case 1:
+            var id = movies.Any() ? movies.Max(m => m.Id) + 1 : 1;
+            Console.WriteLine("Please enter the name of the Movie!");
+            var name = Console.ReadLine();
+            while (string.IsNullOrEmpty(name))
             {
-                Console.WriteLine("Please Choose the action you want to perform !");
-                Console.WriteLine("1: Create item");
-                Console.WriteLine("2: Update item");
-                Console.WriteLine("3: Delete item");
-                Console.WriteLine("4: Display item");
-                Console.WriteLine("5: Exit \n");
+                Console.WriteLine("Name can't be null!");
+                Console.WriteLine("Please enter the name of the Movie!");
+                name = Console.ReadLine();
             }
 
-            do
+            Console.WriteLine("Please enter the genre of the Movie !");
+            string genre = Console.ReadLine();
+
+            Console.WriteLine("Please enter the year of the Movie !");
+            int year = int.Parse(Console.ReadLine());
+
+            Movie m = new Movie(id, name, genre, year);
+            movies.Add(m);
+
+
+            break;
+
+        case 2:
+            Console.WriteLine("Please enter the name of the Movie you want to Change!");
+            string oldname = Console.ReadLine();
+
+
+            Console.WriteLine("Please enter the new name: ");
+            string newName = Console.ReadLine();
+            Console.WriteLine("Please enter the new genre: ");
+            string newGenre = Console.ReadLine();
+            Console.WriteLine("Please enter the new release year: ");
+            int newYear = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            var movie = movies.FirstOrDefault(m => m.Name == oldname);
+
+            foreach (Movie movie in movies.ToList())
             {
-                printOptions();
-                choice = int.Parse(Console.ReadLine());
-
-                switch (choice)
+                if (movie.Name == oldname)
                 {
-                    case 1:
-                        Console.WriteLine("Please enter the name of the Movie!");
-                        string name = Console.ReadLine();
+                    movie.Update(newName, newGenre, newYear);
+                    counter++;
+                    Console.WriteLine("Movie updated successfuly");
+                    break;
+                }
+            }
 
-                        Console.WriteLine("Please enter the genre of the Movie !");
-                        string genre = Console.ReadLine();
-
-                        Console.WriteLine("Please enter the year of the Movie !");
-                        int year = int.Parse(Console.ReadLine());
-
-                        Movie m = new Movie(name, genre, year);
-                        list.Add(m);
+            if (counter == 0) Console.WriteLine("Movie not found ya errrr !!!");
 
 
-                        break;
+            break;
 
-                     case 2:
-                     Console.WriteLine("Please enter the name of the Movie you want to Change!");
-                     string oldname = Console.ReadLine();
+        case 3:
+            Console.WriteLine("Please enter the name of the Movie you want to Delete !");
+            string Dname = Console.ReadLine();
 
-                  
-                            Console.WriteLine("Please enter the new name: ");
-                            string newName = Console.ReadLine();
-                            Console.WriteLine("Please enter the new genre: ");
-                      string newGenre = Console.ReadLine();
-                     Console.WriteLine("Please enter the new release year: ");
-                      int newYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the genre of the Movie you want to Delete !");
+            string Dgenre = Console.ReadLine();
 
-                            int counter = 0;
+            Console.WriteLine("Please enter the year of the Movie you want to Delete !");
+            int Dyear = int.Parse(Console.ReadLine());
 
-                            foreach (Movie movie in list.ToList())
-                            {
-                                if (movie.name == oldname)
-                                {
-                                    movie.name = newName;
-                                    movie.genre = newGenre;
-                                    movie.year = newYear;
-                                    counter++;
-                                    Console.WriteLine("Movie updated successfuly");
-                                    break;
-                                }
-                            }
-
-                            if (counter == 0) Console.WriteLine("Movie not found ya errrr !!!") ;
-
-
-                     break;
-
-                    case 3:
-                        Console.WriteLine("Please enter the name of the Movie you want to Delete !");
-                        string Dname = Console.ReadLine();
-
-                        Console.WriteLine("Please enter the genre of the Movie you want to Delete !");
-                        string Dgenre = Console.ReadLine();
-
-                        Console.WriteLine("Please enter the year of the Movie you want to Delete !");
-                        int Dyear = int.Parse(Console.ReadLine());
-
-                        foreach (var i in list.ToList())
-                        {
-                            if (i.name == Dname && i.genre == Dgenre && i.year == Dyear)
-                            {
-                                list.Remove(i);
-                                break;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Movie not found!");
-                            }
-
-
-                        }
-                        break;
-
-
-                    case 4:
-                        foreach (var i in list)
-                        {
-                            Console.WriteLine("List of Movies: {0} {1} {2}", i.name, i.genre, i.year);
-                        }
-
-
-                        break;
-
-
+            foreach (var i in movies.ToList())
+            {
+                if (i.Name == Dname && i.Genre == Dgenre && i.Year == Dyear)
+                {
+                    movies.Remove(i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Movie not found!");
                 }
 
-            } while (choice != 5);
+
+            }
+            break;
 
 
+        case 4:
+            foreach (var i in movies)
+            {
+                Console.WriteLine("List of Movies: {0} {1} {2}", i.Name, i.Genre, i.Year);
+            }
 
 
+            break;
 
-        }
 
     }
-}
+
+} while (choice != 5);
+
+
+
+
+
